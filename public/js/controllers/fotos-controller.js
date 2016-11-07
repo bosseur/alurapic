cec9209eb15b9910ff992897d21f4eea97fe9e74
ekoetsier <ernst.koetsier@sicpa.com>
2016-11-07 16:36:03 -0200

    aula 3

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,20 @@
-angular.module('alurapic').controller('FotosController', function($scope){
+angular.module('alurapic').controller('FotosController', function($scope, $http){
 
-	$scope.foto={
-		titulo: 'Leao',
-		url:'http://www.fundosanimais.com/Minis/leoes.jpg'
-	}
+	$scope.fotos=[];
+
+	$http.get('v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	})
+	.error(function(error){
+		console.log(error);
+	})
+
+/*
+	promise.then(function(result){
+		$scope.fotos = result.data;
+	}).catch(function(error){
+		console.log(error);
+	});
+*/
 });
